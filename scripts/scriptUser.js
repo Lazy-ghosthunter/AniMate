5e01840066509574af5a487679c4e92d99187970
Ana <anakarolinesantana820@gmail.com>
2025-10-10 06:58:15 -0300

    descrição user

--- scripts/scriptUser.js
+++ scripts/scriptUser.js
@@ -10,4 +10,70 @@ document.addEventListener('DOMContentLoaded', () => {
   if (username && usernameElement) {
     usernameElement.textContent = `@${username}`;
   }
+
+
+  //* lógicas da Bio
+
+  const loadProfile = async () => {
+    const token = localStorage.getItem("token");
+    if (!token) return;
+
+    try {
+      const response = await axios.get(`${base_url}/profile/me?token=${token}`);
+      const userProfile = response.data;
+      
+      const bioP = document.getElementById('bio'); 
+      if (bioP && userProfile.bio) {
+        bioP.textContent = userProfile.bio;
+      }
+    } catch (error) {
+      console.error("Erro ao carregar perfil:", error);
+      alert("Não foi possível carregar as informações do perfil.");
+    }
+  };
+
+  loadProfile();
+
+
+  const toggleBioAlt = () => {
+    const bioAltArea = document.getElementById('bioAltArea');
+    const bioArea = document.getElementById('bioArea');
+    bioAltArea.classList.toggle('hidden');
+    bioArea.classList.toggle('hidden')
+  };
+
+  const toggleBtn = document.getElementById('toggleChange');
+  toggleBtn.addEventListener('click', toggleBioAlt);
+
+
+  const altBio = async () => {
+  const bio = document.getElementById("bioAlt").value.trim();
+  const token = localStorage.getItem("token");
+  
+  const body = {
+    bio: bio, 
+  };
+
+    try {
+      const response = await axios.put(`${base_url}/profile/bio?token=${token}`, body);
+      console.log(response.data);
+      alert("Bio alterada com sucesso");
+
+      window.location.reload();
+
+    } catch (error) {
+      console.error("Erro na alteração: ", error);
+      errorMessage = typeof error.response.data === 'string' ? error.response.data : JSON.stringify(error.response.data);
+      alert(`Falha na alteração: ${errorMessage}`);
+    }
+  
+};
+
+const save = document.getElementById('change');
+save.addEventListener('click', altBio);
+
+
+  
 });
+
+
