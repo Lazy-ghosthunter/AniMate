ebb8b871704c86d64445d241868fbfa9c59e3b76
Ana Karoline Santana <100864173+Lazy-ghosthunter@users.noreply.github.com>
2026-03-28 16:16:28 -0300

    Remove environment variable checks for Chibisafe
    Removed checks for ANIMATE_CHIBISAFE_URL and ANIMATE_CHIBISAFE_API_KEY.

--- build.sh
+++ build.sh
@@ -7,17 +7,7 @@ if [ -z "$ANIMATE_API_URL" ]; then
   echo "Error: ANIMATE_API_URL environment variable is not set." >&2
   exit 1
 fi
-if [ -z "$ANIMATE_CHIBISAFE_URL" ]; then
-  echo "Error: ANIMATE_CHIBISAFE_URL environment variable is not set." >&2
-  exit 1
-fi
-if [ -z "$ANIMATE_CHIBISAFE_API_KEY" ]; then
-  echo "Error: ANIMATE_CHIBISAFE_API_KEY environment variable is not set." >&2
-  exit 1
-fi
 
 cat > scripts/config.js << EOF
 const base_url = "${ANIMATE_API_URL}";
-const chibisafeUrl = "${ANIMATE_CHIBISAFE_URL}";
-const chibisafeApiKey = "${ANIMATE_CHIBISAFE_API_KEY}";
 EOF
